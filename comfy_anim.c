ed9b48bc00a1b00ccd7174bb963cf63fd6971bbf
Eduardo Quezada <eduardo602002@gmail.com>
2026-03-08 10:24:02 -0300

    Remove unused variable in ComfyAnimEasing_EaseInOutBack

--- comfy_anim.c
+++ comfy_anim.c
@@ -270,21 +270,20 @@ s32 ComfyAnimEasing_EaseInOutCubic(s32 t /* Q_24_8 */)
 
 s32 ComfyAnimEasing_EaseInOutBack(s32 t /* Q_24_8 */)
 {
-    s32 c1 = 0x1B3; // Q_24_8 representation of 1.70158
-    s32 c2 = 0x298; // Q_24_8 representation of 1.70158 * 1.525
+    s32 c1 = 0x298; // Q_24_8 representation of 1.70158 * 1.525
 
     if (t < (Q_24_8(1) >> 1))
     {
         s32 v1 = 2 * t;
         s32 a = MathUtil_Mul32(v1, v1);
-        s32 b = MathUtil_Mul32(2 * (c2 + Q_24_8(1)), t) - c2;
+        s32 b = MathUtil_Mul32(2 * (c1 + Q_24_8(1)), t) - c1;
         return (MathUtil_Mul32(a, b)) >> 1;
     }
     else
     {
         s32 v1 = 2 * t - Q_24_8(2);
         s32 a = MathUtil_Mul32(v1, v1);
-        s32 b = MathUtil_Mul32(c2 + Q_24_8(1), v1) + c2;
+        s32 b = MathUtil_Mul32(c1 + Q_24_8(1), v1) + c1;
         return (MathUtil_Mul32(a, b) + Q_24_8(2)) >> 1;
     }
 }
